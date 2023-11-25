61ab39127c03fec93afa1cb98eaa1381c9b4013a
jardsonalan <jardsonalan132@gmail.com>
2023-11-25 18:48:49 -0300

    Adicionei o Método Get para pegar os dados das empresas e ajeitei a conectividade com o banco.

--- Controllers/EmpresaController.cs
+++ Controllers/EmpresaController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using apiMotorcycle.Context;
 using apiMotorcycle.Model;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,10 +13,23 @@ namespace apiMotorcycle.Controllers
     public class EmpresaController : ControllerBase
     {
         private readonly ILogger<Empresas> _logger;
+        private readonly EmpresaContext _context;
 
-        public EmpresaController(ILogger<Empresas> logger)
+        public EmpresaController(ILogger<Empresas> logger, EmpresaContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet(Name = "empresas")]
+
+        public ActionResult<IEnumerable<Empresas>> Get()
+        {
+            var empresas = _context.Empresa.ToList();
+            if(empresas is null)
+                return NotFound();
+            
+            return empresas;
         }
     }
 }
